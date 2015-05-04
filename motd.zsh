8a87d0eaea08b2cee06a2fe67c2d916ee2149d61
Jason Walker <jwwalker@amazon.com>
2015-05-04 10:46:54 -0700

    Recursive cowsay...

--- motd.zsh
+++ motd.zsh
@@ -6,3 +6,11 @@ function motd() {
     echo "AMZN: \$$(stockquote AMZN)\n"
     wu
 }
+
+function recursive_cow() {
+    out=$(cowsay $1)
+    for i in {1..$2}; do
+        out=`echo $out | cowsay -n`
+    done
+    echo $out
+}
