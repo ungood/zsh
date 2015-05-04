0ab303d7c4bcc5db6f7f503783373f92318eff46
Jason Walker <jwwalker@amazon.com>
2015-05-04 10:40:59 -0700

    Better rgit

--- aliases.zsh
+++ aliases.zsh
@@ -23,8 +23,15 @@ alias today='date -u +%F'
 
 # Executes a git command on all git directories under the current one.
 function rgit() {
-    find -L . -maxdepth 4 -type d -name \.git -print -exec git -C "{}/.." "$@" \;
+    gitdirs=(`find -L . -maxdepth 4 -type d -name \.git -print`)
+    for d in $gitdirs; do
+        repo_dir=`dirname $d`
+        repo_name=`basename $repo_dir`
+        echo "\n# ${repo_name}"
+        git -C "$repo_dir" $@
+    done
 }
+alias rst='rgit status -sb'
 
 function update-all() {
     antigen selfupdate
