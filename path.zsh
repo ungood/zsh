540815e1ccabb448c581d81437820facd66b0a84
Jason Walker <jason@onetrue.name>
2021-10-22 12:54:32 -0700

    append_path_if function

--- path.zsh
+++ path.zsh
@@ -1,2 +1,11 @@
-path+=(~/bin)
-export path
+function append_path_if() {
+    if [[ -d "$1" ]]; then
+        path+=("$1")
+        export path
+    fi
+}
+
+append_path_if "~/bin"
+append_path_if "~/.poetry/bin"
+append_path_if "~/.toolbox/bin"
+append_path_if "/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
