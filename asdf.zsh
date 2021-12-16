047417f4ca7ec0cf423f5cbb34842d3e2e3d6490
Jason Walker <jason@onetrue.name>
2021-12-16 10:25:20 -0800

    fixes theme and updates asdf

--- asdf.zsh
+++ asdf.zsh
@@ -1,11 +1,8 @@
-if [ -x "$(command -v asdf)" ]; then
-    if asdf current direnv &> /dev/null; then
-        eval "$(asdf exec direnv hook zsh)"    
-        direnv() { asdf exec direnv "$@"; }
-    else
-        echo "direnv not installed:"
-        asdf current direnv
-    fi
+
+if asdf current direnv &> /dev/null; then
+    eval "$(asdf exec direnv hook zsh)"
+    alias direnv='asdf exec direnv'
 else
-    echo "asdf is not installed."
+    echo "direnv not installed:"
+    asdf current direnv
 fi
